8e98af4b0db22b27ed2130a6ccfea030bb905c57
radicalmachine <edwdzzzytt@gmail.com>
2023-01-25 10:34:01 +0000

    project setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+
+//* PLEASE USE TYPESCRIPT OF WORKSPACE VERSION:
+//* VScode? F1=>TypeScript:Select typeScript version=>Use Workspace Version
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
